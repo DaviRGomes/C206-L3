8a78a1008b978292ab52c1514b4569d440d45d5d
DaviRGomes <davirosagomesvini@gmail.com>
2023-03-04 21:33:11 -0300

    .

--- aula_01_03/src/Imc.java
+++ aula_01_03/src/Imc.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Imc {
 
     public static void main(String[] args) {
@@ -28,30 +31,36 @@ public class Imc {
 
         imc = peso/soma;
 
+        // metodo para arredondar o decimal
+        BigDecimal res = new BigDecimal(imc);
+        // 2 indica quantidade de casas depois da virgula
+        // RoundingMode.HALF_UP metodo padrão para o arredondamento
+        res = res.setScale(2, RoundingMode.HALF_UP);
+
         // saida
 
         if (imc <= 18.5) {
-            System.out.println("O seu IMC eh: " + imc + " voce esta abaixo do peso");
+                System.out.println("O seu IMC eh: " + res + " voce esta abaixo do peso");
         }
 
         else if ((imc >= 18.5) && (imc <= 24.9)) {
-            System.out.println("O seu IMC eh: " + imc + " voce esta no peso ideal");
+            System.out.println("O seu IMC eh: " + res + " voce esta no peso ideal");
         }
 
         else if ((imc >= 24.9) && (imc <= 29.9) ){
-            System.out.println("O seu IMC eh: " + imc + " voce esta levemente acima do peso");
+            System.out.println("O seu IMC eh: " + res + " voce esta levemente acima do peso");
         }
 
         else if ((imc >= 30.0) && (imc <= 34.9) ){
-            System.out.println("O seu IMC eh: " + imc + " voce esta com obesidade de grua I");
+            System.out.println("O seu IMC eh: " + res + " voce esta com obesidade de grua I");
         }
 
         else if ((imc >= 35.0) && (imc <= 39.9) ){
-            System.out.println("O seu IMC eh: " + imc + " voce esta com obesidade de grua II (severa)");
+            System.out.println("O seu IMC eh: " + res + " voce esta com obesidade de grua II (severa)");
         }
 
         else
-            System.out.println("O seu IMC eh: " + imc + " voce esta com obesidade de grua III (mórbida)");
+            System.out.println("O seu IMC eh: " + res + " voce esta com obesidade de grua III (mórbida)");
 
     }
 }
